60fd055398e36619fe869064a22707be4ee5f4cd
karhong-sam <karhongsam@gmail.com>
2022-05-25 16:15:20 +0800

    initial commit

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+
+from app.torch_utils import transform_image, get_prediction
+
+app =Flask(__name__)
+
+ALLOWED_EXTENSIONS = {"png", "jpeg", "jpg"}
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if request.method == "POST":
+        file = request.files.get("file")
+        if file is None or file.filename == "":
+            return jsonify({"Error": "No file found"})
+        if not allowed_file(file.filename):
+            return jsonify({"Error": "Format not supported"})
+    
+        try:
+            img_bytes = file.read()
+            tensor = transform_image(img_bytes)
+            prediction = get_prediction(tensor)
+            data = {"prediction": prediction.item(), "class_name": str(prediction.item())}
+            return jsonify(data)
+            
+        except:
+            return jsonify({"Error": "Error during prediction"})
+
